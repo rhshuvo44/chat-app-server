1667faef05e96eb58d8c2ad95929f22317a734ae
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-04 15:38:31 +0600

    message model

---
+++ models/message.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+var messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps,
+  }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
