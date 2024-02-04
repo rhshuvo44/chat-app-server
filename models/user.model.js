a9f93a6e6ddd034792f90499e051b692b8ea4d23
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-04 14:08:34 +0600

    user model

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  profilePic: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
