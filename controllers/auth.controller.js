f64ab1fc35d317d57b25185d603b5714e3691d17
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-06 12:13:29 +0600

    log out fuction update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -63,11 +63,11 @@ const login = async (req, res) => {
 };
 const logout = (req, res) => {
   try {
-    req.cookie("jwt", "", { maxAge: 0 });
-    return res.status(200).json({ message: "Logged out successfully" });
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error is logout controller", error.message);
-    res.status(500).json({ error: "Internal server Error" });
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
